a3862a28a05eea0c7699e22e16644a24da3f288f
VISHAL KUMAR <vishalkr12012001@gmail.com>
2025-03-02 07:15:33 +0530

    array

---
+++ week-05/2-javascript conditional and loops/5-array.js
@@ -0,0 +1,9 @@
+let fruit = ["apple", "cherry", "banana"];
+
+let inFruit = new Array("kiwi", "avacado", "dragon fruit");
+
+fruit[0] = "blueberry";
+console.log(fruit.length);
+
+fruit.unshift("new item");
+fruit.pop();
